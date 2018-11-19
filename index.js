2d962df508658369a44ed32ccc31b994b907329e
Maryam Shitu <maryamshitu@gmail.com>
2018-11-19 15:10:30 -0800

    basic clock function working

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function updateClock() {
+  let currentTime = new Date();
+
+  let currentHours = currentTime.getHours();
+  let currentMinutes = currentTime.getMinutes();
+  let currentSeconds = currentTime.getSeconds();
+
+  // Compose the string for display
+  let currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+
+
+  $("#clock").html(currentTimeString);
+}
+
+$(document).ready(function() {
+  setInterval('updateClock()', 1000);
+});
+// 
